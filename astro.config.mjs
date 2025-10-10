af5ce08153bfcfa766e846b7923c061dac8a49f9
David Acevedo <acevedo.posada@hotmail.com>
2025-10-09 22:14:35 -0500

    🚧 Setup project blogs definition

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: node({ mode: "standalone" }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
