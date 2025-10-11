44b8d3bb70f1262641ef63b02551dfe3c73ed5c4
David Acevedo <acevedo.posada@hotmail.com>
2025-10-11 13:26:00 -0500

    🐛 Fix render on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 import path from 'node:path';
 
@@ -13,7 +13,7 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'server',
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
