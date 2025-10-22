84d85fbe0565f0e6322f072a430ade40b18000b9
David Acevedo <acevedo.posada@hotmail.com>
2025-10-21 21:14:45 -0500

    ✨ Improve post header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '| David Acevedo';
+export const SITE_DESCRIPTION = 'Thought Journal — a personal tech & lifestyle blog where software development insights, the latest dev news, and everyday life meet to inspire and inform curious creators.';
